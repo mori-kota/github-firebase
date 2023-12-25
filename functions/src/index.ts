13553b7d02b3ca6ed8c9bc2da1603f58d0f331b9
mori-kota <idein.sky@gmail.com>
2023-12-25 11:29:47 +0900

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+import * as functions from 'firebase-functions';
+import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const httpFunction = functions.https.onRequest((request: functions.Request, response: functions.Response) => {
+    const body = request.body;
+    console.log("body", body);
+
+    // Numbers passed from the client.
+    const firstNumber = body.firstNumber;
+    const secondNumber = body.secondNumber;
+  
+    // Checking that attributes are present and are numbers.
+    if (!Number.isFinite(firstNumber) || !Number.isFinite(secondNumber)) {
+      response.status(400);
+    }
+  
+    const res = {
+      firstNumber: firstNumber,
+      secondNumber: secondNumber,
+      operator: "+",
+      operationResult: firstNumber + secondNumber,
+    }
+
+    response.status(200).send(res);
+});
